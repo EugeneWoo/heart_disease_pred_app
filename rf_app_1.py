f4a034c105fa27b049fe23de940a79489bee4b46
Eugene Woo <EugeneWoo@users.noreply.github.com>
2023-12-20 17:21:21 +0000

    Add files via upload

--- rf_app_1.py
+++ rf_app_1.py
@@ -22,45 +22,55 @@ st.set_page_config(page_title="Heart Disease Psychic", layout="wide")
 
 
 # Take input from users for numerical features  
+input_array = np.empty((1,4))
 
-def main():
-    with st.form("road_traffic_severity_form"):
-        age_option = st.sidebar.slider("What is your age?", 25, 80, value=25)
-        st.write('You are ', age_option, ' years old.')
-
-        choices = {0: "Male", 1: "Female"}
-        def format_func(option):
-            return choices[option]
-
-        sex_option = st.sidebar.selectbox("What is your gender? Select 0 for male and 1 for female.", options=list(choices.keys()), format_func=format_func)
-        st.write(f"You selected option {sex_option} representing {format_func(sex_option)}")
-
-        bp_option = st.sidebar.slider("What is your systolic blood pressure in mmHg?", 90, 210, value=90)
-        st.write("Your systolic blood pressure is", bp_option, "mmHg.")
-
-        cholestrol_option = st.sidebar.slider("What is your total cholesterol level in mg/dL?", 120, 570, value=120)
-        st.write("Your total cholesterol is", cholestrol_option, "mg/dL.")
+# def main():
+with st.form("Personal_Particulars"):
+             
+    choices = {0: "male", 1: "female"}
+    def format_func(option):
+        return choices[option]
         
-        # User hit submit button to predict heart disease
-        submit = st.sidebar.form_submit_button("Predict")
+    age_option = st.sidebar.slider("What is your age?", 25, 80, value=25)
+    sex_option = st.sidebar.selectbox("What is your gender?", options=list(choices.keys()), format_func=format_func)
+    bp_option = st.sidebar.slider("What is your systolic blood pressure in mmHg?", 90, 210, value=90)
+    cholestrol_option = st.sidebar.slider("What is your total cholesterol level in mg/dL?", 120, 570, value=120)
 
-    if submit:
-        input_array = np.array([age_option,sex_option,bp_option,cholestrol_option], ndmin=2)
+    st.header('**Just confirming your selection**', divider='rainbow')
 
-    # predict the output using model object
-    prediction = model.predict(input_array)[0]
+    st.write('You are ', age_option, ' years old.')
+    # st.write(f"You selected option {sex_option} representing {format_func(sex_option)}")
+    st.write(f"You are a {format_func(sex_option)}.")
+    st.write("Your systolic blood pressure is", bp_option, "mmHg.")
+    st.write("Your total cholesterol is", cholestrol_option, "mg/dL.")
+        
+    # User to submit options to predict heart disease
+    submit = st.form_submit_button("Predict")
+
+if submit:
+    input_array = np.array([age_option,sex_option,bp_option,cholestrol_option], ndmin=2)
     
-    def hd_predict(prediction):
-        if prediction == 0:
-            hd_pred = 'living a life free of heart disease.'
-            pred_prob = rf_best.predict_proba(input_1)[0][prediction]
-        else: 
-            hd_pred = 'suffering from heart disease in future.'
-            pred_prob = round(rf_best.predict_proba(input_1)[0][prediction],2)*100
-        return(hd_pred,pred_prob)
+    # user_data = pd.DataFrame({
+    #     "age": [age_option],
+    #     "sex": [sex_option],
+    #     "BP": [bp_option],
+    #     "cholestrol": [cholestrol_option]
+    #     })
 
-    st.write("You have a",hd_predict(prediction)[1],"% probability of",hd_predict(prediction)[0])
+# predict the output using model object
+prediction = model.predict(input_array)[0]
 
-if __name__ == '__main__':
-    main()
+def hd_predict(prediction):
+    if prediction == 0:
+        hd_pred = 'live a life free of heart disease'
+        pred_prob = model.predict_proba(input_array)[0][prediction]
+    else: 
+        hd_pred = 'suffer from heart disease in future'
+        pred_prob = model.predict_proba(input_array)[0][prediction]
+    return(hd_pred,pred_prob)
+
+st.write("You are expected to",hd_predict(prediction)[0],"with a probability of",round(hd_predict(prediction)[1],2)*100,"%.")
+
+# if __name__ == '__main__':
+#     main()
 
